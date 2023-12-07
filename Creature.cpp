d7c1fcc08dd8482cbd9216e29d8eceaf486cad74
SejalBehere <123218506+SejalBehere@users.noreply.github.com>
2023-12-07 14:24:02 -0500

    Create Creature.cpp and define member functions

---
+++ Creature.cpp
@@ -0,0 +1,28 @@
+#include "Creature.h"
+
+Creature::Creature(string creature_symbol, int creature_x, int creature_y):FieldInhabitant(creature_symbol) //Constructor definition
+{
+
+}
+
+// Getter function definitions
+int Creature::getCreature_x() 
+{
+    return creature_x;
+}
+
+int Creature::getCreature_y() 
+{
+    return creature_y;
+}
+
+// Setter function definitions
+void Creature::setCreature_x(int creature_x)
+{
+    this->creature_x = creature_x;
+}
+
+void Creature::setCreature_y(int creature_y)
+{
+    this->creature_y = creature_y;
+}
