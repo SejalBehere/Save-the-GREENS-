eabc26cb4093974084106c83109f82b2b409ed6a
SejalBehere <123218506+SejalBehere@users.noreply.github.com>
2023-12-07 18:52:23 -0500

    Update Captain.cpp to define the constructor, member functions, getter and setter functions

--- Captain.cpp
+++ Captain.cpp
@@ -1,17 +1,16 @@
 #include "Captain.h"
 
-Captain::Captain(int captain_x, int captain_y) : Creature("V", captain_x, captain_y) //Constructor defintion
+Captain::Captain(int captain_x, int captain_y) : Creature("V", captain_x, captain_y)
 {
     this->captain_x = captain_x;
     this->captain_y = captain_y;     
 }
 
-void Captain::addVeggie(Veggie* veggie) //Member funtion definition
+void Captain::addVeggie(Veggie* veggie)
 {
     collected_Veggies.push_back(veggie); 
 }
 
-//Getter funtion definitions
 int Captain::getcaptain_x()
 {
     return captain_x;
@@ -22,16 +21,22 @@ int Captain::getcaptain_y()
     return captain_y;
 }
 
-// Member funtion
 vector<Veggie*> Captain::getCollected_Veggies()
 {
     return collected_Veggies;
 }
 
-// Setter funtion definitions
-void Captain::setCollected_Veggies()
+void Captain::setCollected_Veggies(bool less_than_five)
 {
-
+    if(less_than_five)
+        collected_Veggies.clear();
+    else
+    {
+        for(int i=0; i<5; i++)
+        {
+            collected_Veggies.pop_back();
+        }
+    }
 }
 
 void Captain::setcaptain_coord(int captain_x, int captain_y)
